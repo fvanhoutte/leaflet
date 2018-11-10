d9262deef745a215acd750ed3f956dc6ead070ae
fvanhoutte <37404404+fvanhoutte@users.noreply.github.com>
2018-11-10 15:31:32 +0100

    Update script.js

--- 0/script.js
+++ 0/script.js
@@ -1,1 +1,6 @@
-
+var map = L.map('map');
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © OpenStreetMap contributors';
+var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
+map.setView([47.0, 3.0], 6);
+map.addLayer(osm);
