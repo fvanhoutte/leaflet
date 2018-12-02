7b23be265ce41b9452622e95fce7aaf82875c46a
fvanhoutte <37404404+fvanhoutte@users.noreply.github.com>
2018-12-02 10:14:25 +0100

    Add files via upload

--- 0/script.js
+++ 0/script.js
@@ -2,5 +2,7 @@ var map = L.map('map');
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © OpenStreetMap contributors';
 var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
-map.setView([47.0, 3.0], 6);
+map.setView([49.4003, 2.8], 16);
 map.addLayer(osm);
+var marker = L.marker([49.4003, 2.8]).addTo(map);
+marker.bindPopup("<b>GSU - UR06</b><br>21/11/2018 - Initiation au websig").openPopup();
